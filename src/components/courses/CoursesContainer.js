ced49ee8acc710671d16225be0528dd74a9ff94b
Dominic Newell <dnewell@netchexonline.com>
2020-08-16 00:16:42 -0500

    Add author reducer and update course list to show author name

--- src/components/courses/CoursesContainer.js
+++ src/components/courses/CoursesContainer.js
@@ -1,25 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import CourseList from "./CourseList";
 
 const CoursesContainer = () => {
   const dispatch = useDispatch();
-  const [course, setCourse] = useState({ title: "" });
+  const [mappedCourses, setMappedCourses] = useState([]);
   const courses = useSelector((state) => state.courses);
+  const authors = useSelector((state) => state.authors);
 
   useEffect(() => {
-    try {
+    if (authors.length === 0) {
+      dispatch(authorActions.loadAuthors());
+    }
+    if (courses.length === 0) {
       dispatch(courseActions.loadCourses());
-    } catch (error) {
-      throw error;
     }
   }, []);
+
+  useEffect(() => {
+    var updatedCourses = courses.map((course) => {
+      return {
+        ...course,
+        authorName: authors.find((a) => a.id === course.authorId).name,
+      };
+    });
+    setMappedCourses(updatedCourses);
+  }, [courses]);
 
   return (
     <>
       <h2>Courses</h2>
-      <CourseList courses={courses} />
+      <CourseList courses={mappedCourses} />
     </>
   );
 };
